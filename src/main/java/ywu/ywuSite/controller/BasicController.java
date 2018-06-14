995ccfa395b8f85ab868a99a8459ce2d332334a6
Infrain <32154417+Infrain@users.noreply.github.com>
2018-06-14 16:32:17 +0800

    Initial commit

---
+++ src/main/java/ywu/ywuSite/controller/BasicController.java
@@ -0,0 +1,22 @@
+package ywu.ywuSite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class BasicController {
+	
+	@ResponseBody
+	@RequestMapping("/")
+	public String home() {
+		return "index.html";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/first")
+	public String first() {
+		return "wahaha";
+	}
+}
